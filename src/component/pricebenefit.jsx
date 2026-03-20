4580f68610a0816ddc97b818b489065dc485c408
Ayomiposy <georgejoshuaayomiposi@gmail.com>
2026-03-20 03:24:34 +0100

    fixed pricing icon size in pricing list

--- src/component/pricebenefit.jsx
+++ src/component/pricebenefit.jsx
@@ -2,20 +2,20 @@ export const PriceBenefits = () => {
   return (
     <div className="flex flex-col gap-2">
       {/* benefits */}
-      <div className="flex gap-3">
-        <img src="/svg/markIcon.svg" alt="mark icon" />
-        <p>Shared Desk</p>
+      <div className="flex gap-3 items-center">
+        <img className="w-6 h-3" src="/svg/markIcon.svg" alt="mark icon" />
+        <p className="">Shared Desk</p>
       </div>
-      <div className="flex gap-3">
-        <img src="/svg/markIcon.svg" alt="mark icon" />
-        <p>High Spedd WiFi</p>
+      <div className="flex gap-3 items-center">
+        <img className="w-6 h-3" src="/svg/markIcon.svg" alt="mark icon" />
+        <p>High Speed WiFi</p>
       </div>
-      <div className="flex gap-3">
-        <img src="/svg/markIcon.svg" alt="mark icon" />
+      <div className="flex gap-3 items-center">
+        <img className="w-6 h-3" src="/svg/markIcon.svg" alt="mark icon" />
         <p>Uninterrupted Power Supply</p>
       </div>
-      <div className="flex gap-3">
-        <img src="/svg/markIcon.svg" alt="mark icon" />
+      <div className="flex gap-3 items-center">
+        <img className="w-6 h-3" src="/svg/markIcon.svg" alt="mark icon" />
         <p>No Commitments or Unnecessary Extras</p>
       </div>
     </div>
